cad3e33625cc6550a208e25b090ab8f863d83999
Tiago Correia <dosaki@users.noreply.github.com>
2023-07-20 09:08:14 +0100

    Added the ability to configure library replacements (#1) VERSION_TAG=0.2.0
    * Added a new CLI option: --replace
    * Refactored cryptography replacement logic
    This is only for Python.
    This version of lasagna will need --replace=cryptography:40.0.2:manylinux2014_x86_64 as this is no longer done by default.

---
+++ dependencies/library_system_information.go
@@ -0,0 +1,28 @@
+package dependencies
+
+import "strings"
+
+type LibrarySystemInformation struct {
+	Name     string
+	Version  string
+	Platform string
+}
+
+func CreateLibrarySystemInformation(colonSeparatedAttributeList string) LibrarySystemInformation {
+	attributes := strings.Split(colonSeparatedAttributeList, ":")
+	return LibrarySystemInformation{
+		Name:     attributes[0],
+		Version:  attributes[1],
+		Platform: attributes[2],
+	}
+}
+
+func ParseLibraryReplacements(unparsedReplacements string) []LibrarySystemInformation {
+	libraryInformationList := []LibrarySystemInformation{}
+
+	libraryInformationStringList := strings.Split(unparsedReplacements, ",")
+	for _, libraryInformation := range libraryInformationStringList {
+		libraryInformationList = append(libraryInformationList, CreateLibrarySystemInformation(libraryInformation))
+	}
+	return libraryInformationList
+}
--- main.go
+++ main.go
@@ -23,15 +23,17 @@ Usage:
   lasagna --help
 
 Options:
-  -h --help           Show this Help.
-  -o --output=<path>  Path to output file [default: ./layer.zip].
-  -e --exclude=<lib>  A library or a comma-separated list of libraries to exclude.
-  -z --nix-zip        Use zip, rather than letting lasagna do it (this is faster).
-  -v --verbose        Extra output for debugging.
+  -h --help               Show this Help.
+  -o --output=<path>      Path to output file [default: ./layer.zip].
+  -e --exclude=<lib>      A library or a comma-separated list of libraries to exclude.
+  -z --nix-zip            Use zip, rather than letting lasagna do it (this is faster).
+  -v --verbose            Extra output for debugging.
+  -r --replace=<library>  Comma-separated list of libraries with a specific version and platform. These will replace the default libraries.
 
 Examples:
   lasagna --output=./my-layer.zip
-  lasagna --output=./my-layer.zip -z --exclude=lib1,lib2`
+  lasagna --output=./my-layer.zip -z --exclude=lib1,lib2
+  lasagna --output=./my-layer.zip -z --replace=lib1:1.0.0:platform,lib2:2.0.0:platform`
 
 	arguments, _ := docopt.ParseDoc(usage)
 	version, _ := arguments.Bool("--version")
@@ -43,9 +45,11 @@ Examples:
 	exclude, _ := arguments.String("--exclude")
 	useSystemZip, _ := arguments.Bool("--nix-zip")
 	isVerbose, _ := arguments.Bool("--verbose")
+	replace, _ := arguments.String("--replace")
 	absoluteOutput, _ := filepath.Abs(output)
 	excludes := helpers.RemoveElements(strings.Split(exclude, ","), "")
 
+	libraryReplacements := dependencies.ParseLibraryReplacements(replace)
 	cwd, err := os.Getwd()
 	helpers.CheckError(err)
 	file := io.FindDependencies(cwd)
@@ -53,7 +57,7 @@ Examples:
 		log.Println("Found dependencies file: " + file)
 	}
 	log.Println("Fetching dependencies...")
-	dependencies.FetchDependencies(file, absoluteOutput+".tmp", excludes, isVerbose)
+	dependencies.FetchDependencies(file, absoluteOutput+".tmp", libraryReplacements, excludes, isVerbose)
 	log.Println("Zipping dependencies...")
 	io.Zip(output+".tmp", absoluteOutput, useSystemZip)
 	os.RemoveAll(absoluteOutput + ".tmp")
